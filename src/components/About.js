6f6a786d1f33de7337da4a33fa90e45978e1b9ff
Jennifer Li <50808350+jennifer-hy-li@users.noreply.github.com>
2022-01-22 23:37:13 -0500

    Adding resume

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hi, I'm Jennifer lin.
+                        Hi, I'm Jennifer Li.
                         <br className="hidden lg:inline-block" /> I love to code and learn!
                     </h1>
                     <p className="mb-8 leading-relaxed">
@@ -15,9 +15,9 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a
-                            href="#contact"
+                            href="https://docs.google.com/document/d/1-6T1vaq-_WpatKnU_09cDmLGj1KaCJWdFPvYi3Uvn8g/edit?usp=sharing"
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                            Work With Me
+                            View my resume
                         </a>
                         <a
                             href="#projects"
