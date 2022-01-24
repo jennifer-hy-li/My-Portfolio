6d578e64a32e16777b376011326051886dc7363e
Jennifer Li <50808350+jennifer-hy-li@users.noreply.github.com>
2022-01-24 01:50:12 -0500

    colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,12 +5,12 @@ import { projects } from "../data";
 
 export default function Projects() {
     return (
-        <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+        <section id="projects" className="text-ebony bg-ash-gray body-font">
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className="flex flex-col w-full mb-20">
                     <CodeIcon className="mx-auto inline-block w-10 mb-4" />
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-                        Apps I've Built
+                        Things I've doo dabed
                     </h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
