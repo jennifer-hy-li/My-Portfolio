622632500d9d5f65c9594dfa94b9e536be7b4812
Jennifer Li <50808350+jennifer-hy-li@users.noreply.github.com>
2022-01-23 23:35:51 -0500

    resume link

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a
-                            href="https://docs.google.com/document/d/1-6T1vaq-_WpatKnU_09cDmLGj1KaCJWdFPvYi3Uvn8g/edit?usp=sharing"
+                            href="./resume.pdf"
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                             View my resume
                         </a>
